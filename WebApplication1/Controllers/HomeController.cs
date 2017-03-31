7348b4091ffd45894f1593889c91315361418542
fucyier <canerdemir2@gmail.com>
2017-03-31 12:16:27 +0300

    sdasdasdads

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Linq;
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guest);
+                return View("Thanks", guest);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses() { return View(Repository.Responses.Where(x => x.WillAttend == true)); }
+
+    }
+}
