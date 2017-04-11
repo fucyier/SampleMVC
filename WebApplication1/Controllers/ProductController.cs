8ab4a1a1f53866b6727d1b69c55c95c0cce4f61e
fucyier <canerdemir2@gmail.com>
2017-04-11 11:54:39 +0300

    11.04

--- WebApplication1/Controllers/ProductController.cs
+++ WebApplication1/Controllers/ProductController.cs
@@ -4,18 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using WebApplication1.Models.ViewModels;
 
 namespace WebApplication1.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
-public ProductController (IProductRepository repo)
-	{
-            repository=repo;
-	}
-        
-        public ViewResult List()=>View(repository.Products);
+        public int PageSize = 4;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(int page = 1)
+            => View(new ProductListViewModel
+            {
+                Products = repository.Products.OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            }
+        );
 
     }
 }
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -59,6 +59,11 @@ Configuration["Data:SportStoreProducts:ConnectionString"]));
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                name: "pagination",
+                template: "Products/Page{page}",
+                defaults: new { Controller = "Product", action = "List" });
+
+                routes.MapRoute(
                 name: "default",
                 template: "{controller=Product}/{action=List}/{id?}");
             });
